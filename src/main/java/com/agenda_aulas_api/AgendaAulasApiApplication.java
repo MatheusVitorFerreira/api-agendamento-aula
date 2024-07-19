d0c93bd1c92cad9d29a6d5c43def1f0c3ccf8163
Matheus Vitor Ferreira <153767841+MatheusVitorFerreira@users.noreply.github.com>
2024-07-19 11:25:35 -0300

    Create API

---
+++ src/main/java/com/agenda_aulas_api/AgendaAulasApiApplication.java
@@ -0,0 +1,13 @@
+package com.agenda_aulas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaAulasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaAulasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agenda_aulas_api/AgendaAulasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agenda_aulas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaAulasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
