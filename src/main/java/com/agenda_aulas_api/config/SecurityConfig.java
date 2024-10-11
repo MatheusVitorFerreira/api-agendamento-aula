816dc5603a2a6a3894aec7aa9f201aa7f9ff0e7f
Matheus Vitor Ferreira <153767841+MatheusVitorFerreira@users.noreply.github.com>
2024-10-11 14:32:07 -0300

    Alterações em SecurityConfig

--- src/main/java/com/agenda_aulas_api/config/SecurityConfig.java
+++ src/main/java/com/agenda_aulas_api/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
@@ -19,6 +20,7 @@ import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
@@ -36,6 +38,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration configuration = new CorsConfiguration();
+                    configuration.applyPermitDefaultValues();
+                    return configuration;
+                }))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/sistema-agendamento-aula/api/v1/token/login").permitAll()
                         .requestMatchers("/sistema-agendamento-aula/api/v1/teacher/**").permitAll()
@@ -45,8 +52,10 @@ public class SecurityConfig {
                         .requestMatchers("/sistema-agendamento-aula/api/v1/lesson/**").permitAll()
                         .requestMatchers("/sistema-agendamento-aula/api/v1/schedule-classes/**").permitAll()
                         .requestMatchers("/sistema-agendamento-aula/api/v1/student/**").permitAll()
-                        .requestMatchers("/sistema-agendamento-aula/api/v1/schedule-classes/**").permitAll()
                         .requestMatchers("/sistema-agendamento-aula/api/v1/token/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**").permitAll() // Permite acesso à documentação
+                        .requestMatchers("/swagger-ui/**").permitAll() // Permite acesso ao Swagger UI
+                        .requestMatchers("/swagger-ui.html").permitAll() // Permite acesso ao Swagger UI HTML
                         .anyRequest().authenticated())
                 .csrf(csrf -> csrf.disable())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
@@ -71,5 +80,10 @@ public class SecurityConfig {
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
-}
 
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/**.html", "/webjars/**", "/swagger-resources/**", "/v2/api-docs/**", "/swagger-ui.html", "/swagger-ui/**");
+    }
+}
