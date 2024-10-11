5911c4af1da4b15ea1f4993cd5d2ff77b5a60eb7
Matheus Vitor Ferreira <153767841+MatheusVitorFerreira@users.noreply.github.com>
2024-10-11 13:35:13 -0300

    Correção Swagger

--- src/main/java/com/agenda_aulas_api/config/SecurityConfig.java
+++ src/main/java/com/agenda_aulas_api/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
@@ -36,6 +37,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration configuration = new CorsConfiguration();
+                    configuration.applyPermitDefaultValues();
+                    return configuration;
+                }))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/sistema-agendamento-aula/api/v1/token/login").permitAll()
                         .requestMatchers("/sistema-agendamento-aula/api/v1/teacher/**").permitAll()
@@ -45,8 +51,10 @@ public class SecurityConfig {
                         .requestMatchers("/sistema-agendamento-aula/api/v1/lesson/**").permitAll()
                         .requestMatchers("/sistema-agendamento-aula/api/v1/schedule-classes/**").permitAll()
                         .requestMatchers("/sistema-agendamento-aula/api/v1/student/**").permitAll()
-                        .requestMatchers("/sistema-agendamento-aula/api/v1/schedule-classes/**").permitAll()
                         .requestMatchers("/sistema-agendamento-aula/api/v1/token/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**").permitAll()  // Permite acesso à documentação
+                        .requestMatchers("/swagger-ui/**").permitAll()    // Permite acesso ao Swagger UI
+                        .requestMatchers("/swagger-ui.html").permitAll()  // Permite acesso ao Swagger UI HTML
                         .anyRequest().authenticated())
                 .csrf(csrf -> csrf.disable())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
@@ -54,6 +62,7 @@ public class SecurityConfig {
 
         return http.build();
     }
+
 
     @Bean
     public JwtDecoder jwtDecoder() {
@@ -72,4 +81,3 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 }
-
